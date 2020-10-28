8e5686bb21d5d24bb895fc4293a18976cd34f794
Jade Suntoro <hjades@live.com>
2020-10-28 20:27:46 +0800

    update svelte to svelte@next 3.29.4

--- snowpack.config.js
+++ snowpack.config.js
@@ -1,38 +1,5 @@
+// Consult https://www.snowpack.dev to learn about these options
 module.exports = {
-  extends: "@snowpack/app-scripts-svelte",
-  mount: {
-    public: "/",
-    src: "/_dist_",
-  },
-  plugins: [
-    "@snowpack/plugin-svelte",
-    "@snowpack/plugin-dotenv",
-    "@snowpack/plugin-typescript",
-    [
-      "@snowpack/plugin-run-script",
-      {
-        cmd: "svelte-check --output human",
-        watch: "$1 --watch",
-        output: "stream",
-      },
-    ],
-  ],
-  install: [
-    /* ... */
-  ],
-  installOptions: {
-    /* ... */
-  },
-  devOptions: {
-    /* ... */
-  },
-  buildOptions: {
-    /* ... */
-  },
-  proxy: {
-    /* ... */
-  },
-  alias: {
-    "@": "./src"
-  },
+	extends: '@sveltejs/snowpack-config',
+	plugins: ['@snowpack/plugin-typescript']
 };
--- src/App.test.ts
+++
@@ -1,8 +0,0 @@
-import App from "@/App.svelte";
-import { render } from "@testing-library/svelte";
-
-test("renders learn svelte link", () => {
-  const { getByText } = render(App);
-  const linkElement: HTMLElement = getByText(/learn svelte/i);
-  expect(linkElement).toBeInTheDocument();
-});
