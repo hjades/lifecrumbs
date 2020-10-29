ce2b19bf5afd9cf039473cca9d99d6e373c20506
Jade Suntoro <hjades@live.com>
2020-10-29 21:38:24 +0800

    add headings css

--- src/components/TailwindCSS.svelte
+++ src/components/TailwindCSS.svelte
@@ -2,4 +2,34 @@
   @tailwind base;
   @tailwind components;
   @tailwind utilities;
+
+  @layer base {
+    .headings {
+      @apply font-normal leading-normal mt-0 mb-2;
+    }
+    h1 {
+      @apply headings text-6xl;
+    }
+    h2 {
+      @apply headings text-5xl;
+    }
+    h3 {
+      @apply headings text-4xl;
+    }
+    h4 {
+      @apply headings text-3xl;
+    }
+    h5 {
+      @apply headings text-2xl;
+    }
+    h6 {
+      @apply headings text-xl;
+    }
+    p {
+      @apply text-base font-light leading-relaxed mt-0 mb-4;
+    }
+    small {
+      @apply font-normal leading-normal mt-0 mb-4;
+    }
+  }
 </style>
--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -6,11 +6,6 @@
   h1 {
     @apply p-5 bg-blue-800;
     color: turquoise;
-    text-transform: uppercase;
-    font-size: 4rem;
-    font-weight: 100;
-    line-height: 1.1;
-    margin: 4rem auto;
     max-width: 14rem;
   }
 
@@ -22,4 +17,5 @@
 </style>
 
 <h1>Hello world! <span class="text-red-200">Jade</span></h1>
+<h2>Hi</h2>
 <Counter />
