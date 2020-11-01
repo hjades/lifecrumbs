f7f8eb978d3ecfdd0428c8dceded68cae6ef9513
Jade Suntoro <hjades@live.com>
2020-11-01 14:26:33 +0800

    add Item and List components

---
+++ src/features/expenses/List.svelte
@@ -0,0 +1,9 @@
+<script lang="ts">
+  import Item from "./Item.svelte";
+
+  const items = [1, 2];
+</script>
+
+{#each items as item}
+  <Item />
+{/each}
--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,16 +1,5 @@
-<style lang="postcss">
-  h1 {
-    @apply p-5 bg-blue-800;
-    color: turquoise;
-    max-width: 14rem;
-  }
+<script lang="ts">
+  import List from "$features/expenses/List.svelte";
+</script>
 
-  @media (min-width: 480px) {
-    h1 {
-      max-width: none;
-    }
-  }
-</style>
-
-<h1>Hello world! <span class="text-red-200">Jade</span></h1>
-<h2>Hi</h2>
+<List />
